b746896ae18e1629dab05d23b2a89ccb5cdb20c8
swang87 <xiaofei.wang@yale.edu>
2017-12-20 11:13:25 -0500

    Converted syntax to require fapply() in all function composition steps.
    A few test cases are in test.R

--- R/pipe.R
+++ R/pipe.R
@@ -4,44 +4,20 @@
   if (!is.function(rhs)) stop("Error: rhs is not a function.")
   rhs(lhs)
 }
+
 `%>>>%` <- function(lhs, rhs) { # lhs, rhs both funcs
-  require(codetools)
-  tmpfun <- function() {}
-  fun_args <- as.list(match.call())
-  lhs <- as.character(as.expression(fun_args[[2]]))
-  rhs <- as.character(as.expression(fun_args[[3]]))
-
-  # process leftside
-  if (grepl("%>>>%", lhs) | substr(lhs, 1, 10) == "(function(") {
-    lhs_string <- eval(parse(text=lhs))
-    lhs_string <- paste(deparse(lhs_string), collapse="\n")
-  } else {
-    body(tmpfun) <-  parse(text = paste0("{",  lhs,  "}"))
-    lhs_fun <- findGlobals(tmpfun, merge=FALSE)$fun[2]
-    lhs_args <- names(formals(lhs_fun))[1]
-    lhs_other_args <- gsub(lhs_fun, "", lhs)
-    lhs_other_args <- substr(lhs_other_args, 2, nchar(lhs_other_args)-1)
-    if(nchar(lhs_other_args) > 0) lhs_other_args <- paste0(", ", lhs_other_args)
-    lhs_string <- paste(lhs_fun, "(", lhs_args, lhs_other_args, ")")
-  }
-
-
-  # fiddle right
-  body(tmpfun) <-  parse(text = paste0("{",  rhs,  "}"))
-  rhs_fun <- findGlobals(tmpfun, merge=FALSE)$fun[2]
-  rhs_args <- gsub(rhs_fun, "", rhs)
-  rhs_args <- substr(rhs_args, 2, nchar(rhs_args)-1)
-  if (nchar(rhs_args) > 0) rhs_args <- paste0(", ",rhs_args)
-
-  left_to_right_arg <- names(formals(rhs_fun))[1]
-  eval(parse(text = paste0("fapply(",rhs_fun, ", ",
+  # LHS should either be a function or a call to fapply()
+  lhs_string <- paste(deparse(lhs), collapse="\n")
+  # RHS should always be a function result of fapply
+  left_to_right_arg <- names(formals(rhs)[1])
+  rhs_string <- paste(deparse(rhs), collapse="\n")
+  eval(parse(text = paste0("fapply(",rhs_string, ", ",
                            left_to_right_arg, " = ", lhs_string,
-                           rhs_args,")")))
+                           ")")))
 }
 #' @export
 `%>%` <- function(lhs, rhs) {
   require(codetools)
-
   fun_args <- as.list(match.call())
 
   # detect if first argument is a data frame
--- docs/test.R
+++ docs/test.R
@@ -1,4 +1,10 @@
 library(fapply)
-
 iris %>% (fapply(head, n = ncol(x)) %>%
-          fapply(tail, n=2))
+          fapply(tail, n=2) %>% fapply(nrow))
+v <- fapply(head, n = ncol(x)) %>%
+    fapply(tail, n=2) %>% fapply(nrow)
+v
+v(iris)
+
+
+list(x = iris, n = 3) %>% (fapply(tail, x=x$x, n=x$n) %>% fapply(head, n=2))
